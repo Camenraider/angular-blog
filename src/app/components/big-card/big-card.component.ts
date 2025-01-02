f6e17f982a56000b48f2a91117583968750894fd
João <joaopaulokimi@gmail.com>
2025-01-02 13:24:18 -0300

    components created with styles

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-big-card',
+  imports: [],
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css',
+})
+export class BigCardComponent {
+  imageURL = "https://archive.org/download/placeholder-image/placeholder-image.jpg";
+}
